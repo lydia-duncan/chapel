7388690b63e581fb26c6eba387074e9e47a7dc11
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2019-04-04 08:26:29 -0700

    Minor fleshing out of the new pass strategy, still lots of work to do.

--- compiler/passes/distributedLibraries.cpp
+++ compiler/passes/distributedLibraries.cpp
@@ -18,7 +18,9 @@
  */
 
 #include "driver.h"
+#include "files.h"
 #include "passes.h"
+#include "symbol.h"
 
 // When compiling with `--library` in settings when Chapel code should be
 // prepped for launching or running in a distributed fashion, we want to create
@@ -27,13 +29,38 @@ void distributedLibraries() {
   // need int argc, char* argv[]
 
   // TODO: extend to support `--no-local` and `CHPL_LAUNCHER` != "none"
-  if (fLibraryCompile && !strcmp(CHPL_COMM, "none")) {
+  if (fLibraryCompile && strcmp(CHPL_COMM, "none") != 0) {
     // Want to have two source files.  One will be the original, but with a
     // different name and a new main routine.  The other will have the same
     // name as the original, but will only contain the exported functions and
     // will replace their body with different code.
     std::string duplicateFile = "cp ";
-    //
+    // TODO: finish making the two programs
 
+    // Get all the exported functions in the program
+    std::vector<FnSymbol*> exported;
+
+    forv_Vec(FnSymbol, fn, gFnSymbols) {
+      if (fn->hasFlag(FLAG_EXPORT)) {
+        if (fn->getModule()->modTag != MOD_INTERNAL)
+          exported.push_back(fn);
+      }
+    }
+    // TODO: compute known "function name"->"id" translation now and determine
+    // way to communicate that to the two programs.
+    // e.g. maybe an array in a .h file that gets put in a known location or
+    // something.
+
+    std::string compileClient = "chpl --library-client";
+    std::string compileServer = "chpl --library-server";
+
+    // TODO: finish the compilation commands (by getting argv information)
+
+    // Is this right?  Might need to do something more complicated to aggregate
+    // pass timing, get lldb/gdb to respect it, etc.
+    runCommand(compileClient);
+    runCommand(compileServer);
+
+    // TODO: clean exit, nothing left to do (likely)
   }
 }
